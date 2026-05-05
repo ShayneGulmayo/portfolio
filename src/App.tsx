3db95c2e6c52c0947852d129ef64d3b3d09cf2fd
Shayne Marie Gulmayo <gulmayoshayne@gmail.com>
2026-05-05 23:44:33 +0800

    edit projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import pfp from "./assets/pfp.png";
 import linkedin from "./assets/linkedin.png";
 import github from "./assets/github-sign.png";
 import Card from "./components/Card";
+import { Button } from "@mantine/core";
 
 function App() {
   const aboutCards = [
@@ -73,16 +74,16 @@ function App() {
           className="flex flex-col lg:h-screen p-20 justify-center gap-10 w-full"
         >
           <div className="flex flex-col lg:flex-row gap-y-5 items-center pt-20">
-            <h1 className="text-[#EB638B] font-bold font-sans italic text-4xl lg:text-5xl basis-1/4">
+            <h1 className="text-[#EB638B] text-shadow-xs text-shadow-[#FFD9DA] font-bold font-sans italic text-4xl lg:text-5xl basis-1/4">
               ABOUT ME
             </h1>
-            <div className="border-y-5 border-[#FFD9DA] flex rounded-lg px-5 basis-3/4"></div>
+            <div className="border-y-5 shadow-sm shadow-black border-[#FFD9DA] flex rounded-lg px-5 basis-3/4"></div>
           </div>
           <div className="flex flex-col w-full h-full gap-10">
             <div className="flex flex-col lg:flex-row">
               <p className="text-[#FFD9DA] font-sans text-xl basis-full lg:basis-1/2 pb-10">
                 I'm a passionate full-stack developer with experience in
-                creating dynamic web applications.
+                creating dynamic web applications and android app development. I have strong skills in UI/UX design. I also do digital drawing as a hobby.
               </p>
             </div>
 
@@ -99,10 +100,28 @@ function App() {
         </section>
         <section id="projects" className="flex flex-col w-full lg:h-screen p-20">
           <div className="flex flex-col lg:flex-row-reverse gap-y-5 gap-x-5 items-center pt-20">
-            <h1 className="text-[#EB638B] justify-end font-bold font-sans italic text-4xl lg:text-5xl basis-1/4">
+            <h1 className="text-[#EB638B] text-shadow-sm text-shadow-black justify-end font-bold font-sans italic text-4xl lg:text-5xl basis-1/4">
               PROJECTS
             </h1>
-            <div className="border-y-5 border-[#FFD9DA] flex rounded-lg px-5 basis-3/4"></div>
+            <div className="border-y-5 shadow-sm shadow-black border-[#FFD9DA] flex rounded-lg px-5 basis-3/4"></div>
+          </div>
+          <div className="flex flex-col  lg:flex-row mt-20 items-center">
+              <div className="bg-[#191516] rounded-t-2xl lg:rounded-s-2xl justify-items-center p-10 lg:basis-1/2">
+                
+              </div>
+              <div className="shadow-gray-800 rounded-b-2xl shadow- shadow-lg bg-linear-to-br from-[#EB638B] to-[#FFD9DA] lg:rounded-e-2xl justify-items-center items-center justify-center p-10 lg:basis-1/2">
+                <p className="text-[#191516] font-semibold font-sans text-lg lg:text-2xl">
+                  StudySync
+                </p>
+                <p className="text-[#191516] font-sans text-md lg:text-xl text-center py-10">
+                  StudySync is an Android-based Learning App with chat room and AI integration to serve as a learning aid for students and learners.
+                </p>
+                <Button variant="outline" color="#191516" className="border-[#191516] hover:bg-[#191516] hover:text-[#EB638B]">
+                  <a href="https://github.com/ShayneGulmayo/StudySync-Flashcard-and-Quiz" target="_blank" rel="noopener noreferrer">
+                    View Project
+                  </a>
+                </Button>
+              </div>
           </div>
         </section>
         <footer id="contact" className="flex flex-row lg:h-screen">
